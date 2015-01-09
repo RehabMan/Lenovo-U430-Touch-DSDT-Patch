8bb69118ff9dd6467e6d61900337755411b9f4c7
RehabMan <racerrehabman@gmail.com>
2015-01-08 17:43:33 -0800

    remove obsolete rPort_AR9280_as_AR946x.kext, it is superceded by FakePCIID_AR9280_as_AR946x.kext

--- patch_hda.sh
+++ patch_hda.sh
@@ -2,7 +2,6 @@
 
 #set -x
 
-codec=ALC283
 unpatched=/System/Library/Extensions
 
 # AppleHDA patching function
@@ -10,6 +9,7 @@ function createAppleHDAInjector()
 {
 # create AppleHDA injector for Clover setup...
     echo -n "Creating AppleHDA injector for $1..."
+    rm -Rf AppleHDA_$1.kext
     cp -R $unpatched/AppleHDA.kext/ AppleHDA_$1.kext
     rm -R AppleHDA_$1.kext/Contents/Resources/*
     rm -R AppleHDA_$1.kext/Contents/PlugIns
@@ -17,7 +17,7 @@ function createAppleHDAInjector()
     rm -R AppleHDA_$1.kext/Contents/MacOS/AppleHDA
     rm AppleHDA_$1.kext/Contents/version.plist
     ln -s /System/Library/Extensions/AppleHDA.kext/Contents/MacOS/AppleHDA AppleHDA_$1.kext/Contents/MacOS/AppleHDA
-    cp ./Resources/layout/*.zlib AppleHDA_$1.kext/Contents/Resources/
+    cp ./Resources_$1/layout/*.zlib AppleHDA_$1.kext/Contents/Resources/
     plist=AppleHDA_$1.kext/Contents/Info.plist
     pattern='s/(\d*\.\d*(\.\d*)?)/9\1/'
     replace=`/usr/libexec/plistbuddy -c "Print :NSHumanReadableCopyright" $plist | perl -p -e $pattern`
@@ -36,9 +36,10 @@ function createAppleHDAInjector()
     /usr/libexec/plistbuddy -c "Delete ':IOKitPersonalities:HDA Hardware Config Resource:PostConstructionInitialization'" $plist
     /usr/libexec/plistbuddy -c "Add ':IOKitPersonalities:HDA Hardware Config Resource:IOProbeScore' integer" $plist
     /usr/libexec/plistbuddy -c "Set ':IOKitPersonalities:HDA Hardware Config Resource:IOProbeScore' 2000" $plist
-    /usr/libexec/plistbuddy -c "Merge ./Resources/ahhcd.plist ':IOKitPersonalities:HDA Hardware Config Resource'" $plist
+    /usr/libexec/plistbuddy -c "Merge ./Resources_$1/ahhcd.plist ':IOKitPersonalities:HDA Hardware Config Resource'" $plist
     echo " Done."
 }
 
-rm -R AppleHDA_$codec.kext
-createAppleHDAInjector "$codec"
+createAppleHDAInjector "ALC283"
+
+createAppleHDAInjector "ALC283b"
