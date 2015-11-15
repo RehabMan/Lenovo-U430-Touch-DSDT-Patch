a37da43fa55d130fec83efc81202108fd0599d5a
RehabMan <racerrehabman@gmail.com>
2015-11-15 10:21:44 -0800

    XHCI only cleanup

--- install_downloads.sh
+++ install_downloads.sh
@@ -146,6 +146,8 @@ if [ $? -ne 0 ]; then
     $SUDO rm -Rf $SLE/BrcmFirmwareData.kext $KEXTDEST/BrcmFirmwareData.kext
     # now using IntelBacklight.kext instead of ACPIBacklight.kext
     $SUDO rm -Rf $SLE/ACPIBacklight.kext $KEXTDEST/ACPIBacklight.kext
+    # since EHCI #1 is disabled, FakePCIID_XHCIMux.kext cannot be used
+    $SUDO rm -Rf $KEXTDEST/FakePCIID_XHCIMux.kext
     # deal with some renames
     if [[ -e $KEXTDEST/FakePCIID_Broadcom_WiFi.kext ]]; then
         # remove old FakePCIID_BCM94352Z_as_BCM94360CS2.kext
