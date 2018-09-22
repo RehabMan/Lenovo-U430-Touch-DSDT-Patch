b4cd9123fc3b74e639bb85f8d26a4cec0eb080b0
RehabMan <racerrehabman@gmail.com>
2018-09-22 07:23:40 -0700

    VoodooPS2Daemon deprecated

--- install_downloads.sh
+++ install_downloads.sh
@@ -258,13 +258,8 @@ for kext in $ESSENTIAL; do
     rm -Rf $EFI/EFI/CLOVER/kexts/Other/IntelGraphicsFixup.kext
 done
 
-# install VoodooPS2Daemon
-echo Installing VoodooPS2Daemon to /usr/bin and /Library/LaunchDaemons...
-cd ./downloads/kexts/RehabMan-Voodoo-*
-$SUDO cp ./Release/VoodooPS2Daemon /usr/bin
-$TAG -a Gray /usr/bin/VoodooPS2Daemon
-$SUDO cp ./org.rehabman.voodoo.driver.Daemon.plist /Library/LaunchDaemons
-$TAG -a Gray /Library/LaunchDaemons/org.rehabman.voodoo.driver.Daemon.plist
-cd ../../..
+# remove VoodooPS2Daemon (deprecated)
+$SUDO rm -f /usr/bin/VoodooPS2Daemon
+$SUDO rm -f /Library/LaunchDaemons/org.rehabman.voodoo.driver.Daemon.plist
 
 fi # "toolsonly"
