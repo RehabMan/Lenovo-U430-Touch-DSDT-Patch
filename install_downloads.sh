decf22dfe24668ee36cc7c14d4e7ea239033de6f
RehabMan <racerrehabman@gmail.com>
2018-09-23 14:59:37 -0700

    refactor to use hack-tools project

--- install_downloads.sh
+++ install_downloads.sh
@@ -1,265 +1,44 @@
 #!/bin/bash
-
 #set -x
 
-SUDO=sudo
-#SUDO='echo #'
-#SUDO=nothing
-TAG=tag_file
-TAGCMD=`pwd`/tools/tag
-SLE=/System/Library/Extensions
-LE=/Library/Extensions
-EXCEPTIONS="Sensors|FakePCIID_BCM57XX|FakePCIID_Intel_GbX|FakePCIID_Intel_HDMI|FakePCIID_Intel_HD_Graphics|FakePCIID_XHCIMux|FakePCIID_Broadcom_WiFi.kext|BrcmPatchRAM|BrcmBluetoothInjector|BrcmFirmwareData|USBInjectAll|WhateverName"
-ESSENTIAL="FakeSMC.kext RealtekRTL8111.kext USBInjectAll.kext Lilu.kext WhateverGreen.kext AppleBacklightInjector.kext IntelBacklight.kext VoodooPS2Controller.kext"
-
-# extract minor version (eg. 10.9 vs. 10.10 vs. 10.11)
-MINOR_VER=$([[ "$(sw_vers -productVersion)" =~ [0-9]+\.([0-9]+) ]] && echo ${BASH_REMATCH[1]})
-
-# install to /Library/Extensions for 10.11 or greater
-if [[ $MINOR_VER -ge 11 ]]; then
-    KEXTDEST=$LE
-else
-    KEXTDEST=$SLE
-fi
-
-# this could be removed if 'tag' can be made to work on old systems
-function tag_file
-{
-    if [[ $MINOR_VER -ge 9 ]]; then
-        $SUDO "$TAGCMD" "$@"
-    fi
-}
-
-function check_directory
-{
-    for x in $1; do
-        if [ -e "$x" ]; then
-            return 1
-        else
-            return 0
-        fi
-    done
-}
-
-function nothing
-{
-    :
-}
-
-function remove_kext
-{
-    $SUDO rm -Rf $SLE/"$1" $LE/"$1"
-}
-
-function install_kext
-{
-    if [ "$1" != "" ]; then
-        echo installing $1 to $KEXTDEST
-        remove_kext `basename $1`
-        $SUDO cp -Rf $1 $KEXTDEST
-        $TAG -a Gray $KEXTDEST/`basename $1`
-    fi
-}
-
-function install_app
-{
-    if [ "$1" != "" ]; then
-        echo installing $1 to /Applications
-        $SUDO rm -Rf /Applications/`basename $1`
-        $SUDO cp -Rf $1 /Applications
-        $TAG -a Gray /Applications/`basename $1`
-    fi
-}
-
-function install_binary
-{
-    if [ "$1" != "" ]; then
-        echo installing $1 to /usr/bin
-        $SUDO rm -f /usr/bin/`basename $1`
-        $SUDO cp -f $1 /usr/bin
-        $TAG -a Gray /usr/bin/`basename $1`
-    fi
-}
-
-function install
-{
-    installed=0
-    out=${1/.zip/}
-    rm -Rf $out/* && unzip -q -d $out $1
-    check_directory $out/Release/*.kext
-    if [ $? -ne 0 ]; then
-        for kext in $out/Release/*.kext; do
-            # install the kext when it exists regardless of filter
-            kextname="`basename $kext`"
-            if [[ -e "$SLE/$kextname" || -e "$KEXTDEST/$kextname" || "$2" == "" || "`echo $kextname | grep -vE "$2"`" != "" ]]; then
-                install_kext $kext
-            fi
-        done
-        installed=1
-    fi
-    check_directory $out/*.kext
-    if [ $? -ne 0 ]; then
-        for kext in $out/*.kext; do
-            # install the kext when it exists regardless of filter
-            kextname="`basename $kext`"
-            if [[ -e "$SLE/$kextname" || -e "$KEXTDEST/$kextname" || "$2" == "" || "`echo $kextname | grep -vE "$2"`" != "" ]]; then
-                install_kext $kext
-            fi
-        done
-        installed=1
-    fi
-    check_directory $out/Release/*.app
-    if [ $? -ne 0 ]; then
-        for app in $out/Release/*.app; do
-            # install the app when it exists regardless of filter
-            appname="`basename $app`"
-            if [[ -e "/Applications/$appname" || -e "/Applications/$appname" || "$2" == "" || "`echo $appname | grep -vE "$2"`" != "" ]]; then
-                install_app $app
-            fi
-        done
-        installed=1
-    fi
-    check_directory $out/*.app
-    if [ $? -ne 0 ]; then
-        for app in $out/*.app; do
-            # install the app when it exists regardless of filter
-            appname="`basename $app`"
-            if [[ -e "/Applications/$appname" || -e "/Applications/$appname" || "$2" == "" || "`echo $appname | grep -vE "$2"`" != "" ]]; then
-                install_app $app
-            fi
-        done
-        installed=1
-    fi
-    if [ $installed -eq 0 ]; then
-        check_directory $out/*
-        if [ $? -ne 0 ]; then
-            for tool in $out/*; do
-                install_binary $tool
-            done
-        fi
-    fi
-}
-
-if [ "$(id -u)" != "0" ]; then
-    echo "This script requires superuser access..."
-fi
-
-
-# unzip/install tools
-check_directory ./downloads/tools/*.zip
-if [ $? -ne 0 ]; then
-    echo Installing tools...
-    cd ./downloads/tools
-    for tool in *.zip; do
-        install $tool
-    done
-    cd ../..
-fi
-
-if [ "$1" != "toolsonly" ]; then
-
-# unzip/install kexts
-check_directory ./downloads/kexts/*.zip
-if [ $? -ne 0 ]; then
-    echo Installing kexts...
-    cd ./downloads/kexts
-    for kext in *.zip; do
-        install $kext "$EXCEPTIONS"
-    done
-    if [[ $MINOR_VER -ge 11 ]]; then
-        # 10.11 needs BrcmPatchRAM2.kext
-        cd RehabMan-BrcmPatchRAM*/Release && install_kext BrcmPatchRAM2.kext && cd ../..
-        # 10.11 needs USBInjectAll.kext
-        cd RehabMan-USBInjectAll*/Release && install_kext USBInjectAll.kext && cd ../..
-        # remove BrcPatchRAM.kext just in case
-        remove_kext BrcmPatchRAM.kext
-        # remove injector just in case
-        remove_kext BrcmBluetoothInjector.kext
-    else
-        # prior to 10.11, need BrcmPatchRAM.kext
-        cd RehabMan-BrcmPatchRAM*/Release && install_kext BrcmPatchRAM.kext && cd ../..
-        # remove BrcPatchRAM2.kext just in case
-        remove_kext BrcmPatchRAM2.kext
-        # remove injector just in case
-        remove_kext BrcmBluetoothInjector.kext
-    fi
-    # this guide does not use BrcmFirmwareData.kext
-    remove_kext BrcmFirmwareData.kext
-    # now using IntelBacklight.kext instead of ACPIBacklight.kext
-    remove_kext ACPIBacklight.kext
-    # since EHCI #1 is disabled, FakePCIID_XHCIMux.kext cannot be used
-    remove_kext FakePCIID_XHCIMux.kext
-    # deal with some renames
-    remove_kext FakePCIID_BCM94352Z_as_BCM94360CS2.kext
-    remove_kext FakePCIID_HD4600_HD4400.kext
-    # IntelGraphicsFixup.kext is no longer used (replaced by WhateverGreen.kext)
-    remove_kext IntelGraphicsFixup.kext
-    # FakePCIID_Intel_HD_Graphics.kext not needed either
-    remove_kext FakePCIID_Intel_HD_Graphics.kext
-    # using AirportBrcmFixup.kext instead of FakePCIID_Broadcom_WiFi.kext
-    remove_kext FakePCIID_Broadcom_WiFi.kext
-    cd ../..
-fi
-
-# install kexts in the repo itself
-
-# patching AppleHDA
+EXCEPTIONS=
+ESSENTIAL="FakePCIID_AR9280_as_AR946x.kext"
 HDA=ALC283
-remove_kext AppleHDA_$HDA.kext
-remove_kext AppleHDAHCD_$HDA.kext
-$SUDO rm -f $SLE/AppleHDA.kext/Contents/Resources/*.zml*
-./patch_hda.sh "$HDA"
-if [[ $MINOR_VER -le 9 ]]; then
-    # dummyHDA configuration
-    make AppleHDA_$HDA.kext
-    install_kext AppleHDA_$HDA.kext
-else
-    # alternate configuration (requires .xml.zlib .zml.zlib AppleHDA patch)
-    #make AppleHDAHCD_$HDA.kext
-    #install_kext AppleHDAHCD_$HDA.kext
-    $SUDO cp AppleHDA_${HDA}_Resources/*.zml* $SLE/AppleHDA.kext/Contents/Resources
-    $TAG -a Gray $SLE/AppleHDA.kext
-fi
 
-# USBXHC_u430 is mostly specific to 10.11, but it does inject non-removable=yes
-# for the touchscreen
-# Note: not used anymore
-#install_kext USBXHC_u430.kext
+# include subroutines
+DIR=$(dirname ${BASH_SOURCE[0]})
+source "$DIR/tools/_install_subs.sh"
+
+warn_about_superuser
+
+# install tools
+install_tools
+
+# install required kexts
+install_download_kexts
+install_brcmpatchram_kexts
+remove_deprecated_kexts
+install_backlight_kexts
+
+# install special download kexts
+install_kext _downloads/kexts/RehabMan-FakePCIID*/Release/FakePCIID_AR9280_as_AR946x.kext
+
+# EHCI is disabled, so no need for FakePCIID_XHCIMux.kext
+remove_kext FakePCIID_XHCIMux.kext
+
+# create/install patched AppleHDA files
+install_hda
+
+# USBXHC_u430 is not used anymore
 remove_kext USBXHC_u430.kext
 
-# install AppleBacklightInjector.kext on 10.12
-#  (set BKLT=1 in SSDT-HACK.dsl to use it, set BKLT=0 to use IntelBacklight.kext)
-if [[ $MINOR_VER -ge 12 ]]; then
-    cd kexts
-    install_kext AppleBacklightInjector.kext
-    cd ..
-    # remove IntelBacklight.kext if it is installed (doesn't work with 10.12)
-    remove_kext IntelBacklight.kext
-fi
+# all kexts are now installed, so rebuild cache
+rebuild_kernel_cache
 
-#check_directory *.kext
-#if [ $? -ne 0 ]; then
-#    for kext in *.kext; do
-#        install_kext $kext
-#    done
-#fi
+# update kexts on EFI/CLOVER/kexts/Other
+update_efi_kexts
 
-# force cache rebuild with output
-$SUDO touch $SLE && $SUDO kextcache -u /
+# VoodooPS2Daemon is deprecated
+remove_voodoops2daemon
 
-# install/update kexts on EFI/Clover/kexts/Other
-EFI=`./mount_efi.sh`
-echo Updating kexts at EFI/Clover/kexts/Other
-for kext in $ESSENTIAL; do
-    echo updating $EFI/EFI/CLOVER/kexts/Other/$kext
-    if [[ -e $KEXTDEST/$kext ]]; then
-        cp -Rfp $KEXTDEST/$kext $EFI/EFI/CLOVER/kexts/Other
-    fi
-    rm -Rf $EFI/EFI/CLOVER/kexts/Other/IntelGraphicsFixup.kext
-done
-
-# remove VoodooPS2Daemon (deprecated)
-$SUDO rm -f /usr/bin/VoodooPS2Daemon
-$SUDO rm -f /Library/LaunchDaemons/org.rehabman.voodoo.driver.Daemon.plist
-
-fi # "toolsonly"
+#EOF
--- makefile
+++ makefile
@@ -6,33 +6,35 @@
 # Created by RehabMan 
 #
 
-BUILDDIR=./build
 HDA=ALC283
 RESOURCES=./Resources_$(HDA)
 HDAINJECT=AppleHDA_$(HDA).kext
-HDAHCDINJECT=AppleHDAHCD_$(HDA).kext
+HDAINJECT_MARK=_hdainject_marker.txt
 HDAZML=AppleHDA_$(HDA)_Resources
-USBINJECT=USBXHC_u430.kext
-BACKLIGHTINJECT=AppleBacklightInjector.kext
-
-VERSION_ERA=$(shell ./print_version.sh)
-ifeq "$(VERSION_ERA)" "10.10-"
-	INSTDIR=/System/Library/Extensions
-else
-	INSTDIR=/Library/Extensions
-endif
-SLE=/System/Library/Extensions
+HDAZML_MARK=_hdazml_marker.txt
 
 # set build products
-PRODUCTS=$(BUILDDIR)/SSDT-HACK.aml
+BUILDDIR=./build
+HDA_PRODUCTS=$(HDAZML_MARK) $(HDAINJECT_MARK)
+AML_PRODUCTS=$(BUILDDIR)/SSDT-HACK.aml
+PRODUCTS=$(AML_PRODUCTS) $(HDA_PRODUCTS)
+
+LE=/Library/Extensions
+SLE=/System/Library/Extensions
+VERSION_ERA=$(shell ./tools/print_version.sh)
+ifeq "$(VERSION_ERA)" "10.10-"
+	INSTDIR=$SLE
+else
+	INSTDIR=$LE
+endif
 
 IASLFLAGS=-vw 2095 -vw 2146
 IASL=iasl
 
 .PHONY: all
-all: $(PRODUCTS) $(HDAHCDINJECT) $(HDAINJECT)
+all: $(PRODUCTS)
 
-$(BUILDDIR)/SSDT-HACK.aml: ./SSDT-HACK.dsl
+$(BUILDDIR)/SSDT-HACK.aml: SSDT-HACK.dsl
 	$(IASL) $(IASLFLAGS) -p $@ $<
 
 .PHONY: clean
@@ -40,73 +42,43 @@ clean:
 	rm -f $(BUILDDIR)/*.dsl $(BUILDDIR)/*.aml
 	make clean_hda
 
-# Clover Install
 .PHONY: install
-install: $(PRODUCTS)
-	$(eval EFIDIR:=$(shell ./mount_efi.sh /))
+install: $(AML_PRODUCTS)
+	$(eval EFIDIR:=$(shell ./mount_efi.sh))
 	rm -f $(EFIDIR)/EFI/CLOVER/ACPI/patched/DSDT.aml
-	rm -f $(EFIDIR)/EFI/CLOVER/ACPI/patched/SSDT-*.aml
-	rm -f $(EFIDIR)/EFI/CLOVER/ACPI/patched/SSDT.aml
-	cp $(PRODUCTS) $(EFIDIR)/EFI/CLOVER/ACPI/patched
+	rm -f $(EFIDIR)/EFI/CLOVER/ACPI/patched/SSDT-*.aml $(EFIDIR)/EFI/CLOVER/ACPI/patched/SSDT.aml
+	cp $(AML_PRODUCTS) $(EFIDIR)/EFI/CLOVER/ACPI/patched
 
-#$(HDAHCDINJECT): $(RESOURCES)/*.plist ./patch_hda.sh
-$(HDAINJECT) $(HDAHCDINJECT): $(RESOURCES)/*.plist ./patch_hda.sh
-	./patch_hda.sh $(HDA)
+$(HDAZML_MARK): $(RESOURCES)/*.plist tools/patch_hdazml.sh tools/_hda_subs.sh
+	./tools/patch_hdazml.sh $(HDA)
+	touch $(HDAZML_MARK)
+
+$(HDAINJECT_MARK): $(RESOURCES)/*.plist tools/patch_hdazml.sh tools/_hda_subs.sh
+	./tools/patch_hdainject.sh $(HDA)
+	touch $(HDAINJECT_MARK)
 
 .PHONY: clean_hda
 clean_hda:
-	rm -rf $(HDAHCDINJECT) $(HDAZML) $(HDAINJECT)
-
-$(BACKLIGHTINJECT): Backlight.plist patch_backlight.sh
-	./patch_backlight.sh
-	touch $@
+	rm -rf $(HDAZML) $(HDAINJECT)
+	rm -f $(HDAZML_MARK) $(HDAINJECT_MARK)
 
 .PHONY: update_kernelcache
-update_kernelcache:
-	sudo touch $(SLE)
-	sudo kextcache -update-volume /
-
-.PHONY: install_hdadummy
-install_hdadummy:
-	sudo rm -Rf $(INSTDIR)/$(HDAINJECT)
-	sudo rm -Rf $(INSTDIR)/$(HDAHCDINJECT)
-	sudo cp -R ./$(HDAINJECT) $(INSTDIR)
-	sudo rm -f $(SLE)/AppleHDA.kext/Contents/Resources/*.zml*
-	if [ "`which tag`" != "" ]; then sudo tag -a Blue $(INSTDIR)/$(HDAINJECT); fi
-	make update_kernelcache
+	update_kernelcache:
+	sudo touch $(SLE) && sudo kextcache -update-volume /
 
 .PHONY: install_hda
 install_hda:
 	sudo rm -Rf $(INSTDIR)/$(HDAINJECT)
-	sudo rm -Rf $(INSTDIR)/$(HDAHCDINJECT)
-	#sudo cp -R ./$(HDAHCDINJECT) $(INSTDIR)
-	#if [ "`which tag`" != "" ]; then sudo tag -a Blue $(INSTDIR)/$(HDAHCDINJECT); fi
 	sudo rm -f $(SLE)/AppleHDA.kext/Contents/Resources/*.zml*
-	sudo cp $(HDAZML)/*.zml* $(SLE)/AppleHDA.kext/Contents/Resources
+	sudo cp $(HDAZML)/* $(SLE)/AppleHDA.kext/Contents/Resources
 	if [ "`which tag`" != "" ]; then sudo tag -a Blue $(SLE)/AppleHDA.kext/Contents/Resources/*.zml*; fi
 	make update_kernelcache
 
-FORCED=/ForcedExtensions
-.PHONY: install_hdax
-install_hdax:
+.PHONY: install_hdadummy
+install_hdadummy:
 	sudo rm -Rf $(INSTDIR)/$(HDAINJECT)
-	sudo rm -Rf $(INSTDIR)/$(HDAHCDINJECT)
-	#sudo cp -R ./$(HDAHCDINJECT) $(FORCED)
-	#if [ "`which tag`" != "" ]; then sudo tag -a Blue $(FORCED)/$(HDAHCDINJECT); fi
-	sudo cp $(HDAZML)/*.zml* $(FORCED)/AppleHDA_Resources
-	if [ "`which tag`" != "" ]; then sudo tag -a Blue $(FORCED)/AppleHDA_Resources/*.zml*; fi
-
-.PHONY: install_usb
-install_usb:
-	sudo rm -Rf $(INSTDIR)/$(USBINJECT)
-	sudo cp -R ./$(USBINJECT) $(INSTDIR)
-	if [ "`which tag`" != "" ]; then sudo tag -a Blue $(INSTDIR)/$(USBINJECT); fi
-	make update_kernelcache
-
-.PHONY: install_backlight
-install_backlight:
-	sudo rm -Rf $(INSTDIR)/$(BACKLIGHTINJECT)
-	sudo cp -R ./$(BACKLIGHTINJECT) $(INSTDIR)
-	if [ "`which tag`" != "" ]; then sudo tag -a Blue $(INSTDIR)/$(BACKLIGHTINJECT); fi
+	sudo cp -R ./$(HDAINJECT) $(INSTDIR)
+	sudo rm -f $(SLE)/AppleHDA.kext/Contents/Resources/*.zml*
+	if [ "`which tag`" != "" ]; then sudo tag -a Blue $(INSTDIR)/$(HDAINJECT); fi
 	make update_kernelcache
 
